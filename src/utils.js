c234f29ee31e6437a0d6cc89e56bdef209b1a43a
silvester-zogaj <silvester_9x@hotmail.co.uk>
2023-06-01 10:56:25 +0100

    created reviewComments branch

--- src/utils.js
+++ src/utils.js
@@ -25,3 +25,14 @@ export const getSingleReview = (review_id) => {
       console.log(err);
     });
 };
+
+export const getReviewComments = (id) => {
+  return ncGamesApi
+    .get(`/reviews/${id}/comments`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
