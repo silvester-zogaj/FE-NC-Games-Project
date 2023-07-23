d14e26e2d61801098e6fb97ff5188d044a6563f6
silvester-zogaj <silvester_9x@hotmail.co.uk>
2023-07-23 10:07:43 +0100

    update width of main pages

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -10,10 +10,12 @@ export function ReviewCard({
 }) {
   return (
     <li key={review_id} className="review-card">
-      <h3 className="review-card-title">
-        <em>{title}</em>
-      </h3>
-      <p className="review-card-username">USER: {owner}</p>
+      <div className="upper-rev-card-container">
+        <h3 className="review-card-title">
+          <em>{title}</em>
+        </h3>
+        <p className="review-card-username">USER: {owner}</p>
+      </div>
       <div className="lower-rev-card-container">
         <p className="review-card-votes">votes: {votes}</p>
         <Link to={`/reviews/${review_id}`} className="nav-link">
